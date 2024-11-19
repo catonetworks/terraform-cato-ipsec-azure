572cf356d902fc673654e036ddc8e0f15503fbab
Brian Anderson <brian.anderson@catonetworks.com>
2024-11-19 13:10:21 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,129 @@
+# CATO IPSec Azure Terraform module
+
+Terraform module which creates an IPsec site in the Cato Management Application (CMA), and a primary and secondary IPsec tunnel from Azure to the Cato platform.
+
+## List of Resources:
+- azurerm_public_ip (vpn_gateway_pip)
+- azurerm_local_network_gateway (cato_pop_primary)
+- azurerm_local_network_gateway (cato_pop_secondary)
+- azurerm_virtual_network_gateway_connection (cato_connection_primary)
+- azurerm_virtual_network_gateway_connection (cato_connection_secondary)
+- azurerm_virtual_network_gateway (vpn_gateway)
+- cato_ipsec_site
+
+## Usage
+
+<details>
+<summary>Required Azure resources for IPSec module</summary>
+
+```hcl
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "azure-rg" {
+  location = var.az_location
+  name     = replace(replace("Your-site-name-VNET", "-", ""), " ", "_")
+}
+
+resource "azurerm_availability_set" "availability-set" {
+  location                     = var.az_location
+  name                         = replace(replace("Your-site-name-availabilitySet", "-", "_"), " ", "_")
+  platform_fault_domain_count  = 2
+  platform_update_domain_count = 2
+  resource_group_name          = azurerm_resource_group.azure-rg.name
+  depends_on = [
+    azurerm_resource_group.azure-rg
+  ]
+}
+
+## Create Network and Subnets
+resource "azurerm_virtual_network" "vnet" {
+  address_space       = [var.native_network_range]
+  location            = var.az_location
+  name                = replace(replace("Your-site-name-vsNet", "-", "_"), " ", "_")
+  resource_group_name = azurerm_resource_group.azure-rg.name
+  depends_on = [
+    azurerm_resource_group.azure-rg
+  ]
+}
+
+resource "azurerm_subnet" "subnet" {
+  address_prefixes     = [var.native_network_range]
+  name                 = "GatewaySubnet"
+  resource_group_name  = azurerm_resource_group.azure-rg.name
+  virtual_network_name = replace(replace("Your-site-name-vsNet", "-", "_"), " ", "_")
+  depends_on = [
+    azurerm_virtual_network.vnet
+  ]
+}
+```
+</details>
+
+Example module usage:
+
+```
+module "ipsec-azure" {
+  source                      = "catonetworks/ipsec-azure/cato"
+  token                       = var.cato_token
+  account_id                  = var.account_id
+  az_location                 = "East US"
+  resource_group_name         = replace(replace("${var.site_name}-VNET", "-", ""), " ", "_")
+  vnet_name                   = replace(replace("${var.site_name}-vsNet", "-", "_"), " ", "_")
+  gateway_subnet_id           = azurerm_subnet.subnet.id
+  vpn_gateway_name            = "my-azure-vpn-gateway"
+  local_network_gateway_name  = "cato-local-network-gateway"
+  site_name                   = "My-Azure-Cato-IPSec-Site-8"
+  site_description            = "TestTFAzureIPSec8"
+  native_network_range        = "172.16.0.0/24"
+  primary_private_cato_ip     = "169.1.1.1"
+  primary_private_site_ip     = "169.1.1.2"
+  primary_cato_pop_ip         = "11.22.33.44" # Your Primary Cato IP
+  primary_public_cato_ip_id   = "31511" # Your Primary Cato IP ID
+  secondary_private_cato_ip   = "169.2.1.1"
+  secondary_private_site_ip   = "169.2.1.2"
+  secondary_cato_pop_ip       = "11.22.33.55" # Your Secondary Cato IP ID
+  secondary_public_cato_ip_id = "31512" # Your Secondary Cato IP ID
+  downstream_bw               = 100
+  upstream_bw                 = 100
+  site_location = {
+    city         = "New York City"
+    country_code = "US"
+    state_code   = "US-NY"
+    timezone     = "America/New_York"
+  }
+}
+
+```
+
+## Alloacted IP Reference
+
+You must first [Allocating two IPs](https://support.catonetworks.com/hc/en-us/articles/4413273467153-Allocating-IP-Addresses-for-the-Account), and retrieve the IPs, and IDs to be used in the module. Use the [Cato CLI](https://github.com/catonetworks/cato-cli) to retrieve the IPs and IDs for an account.
+
+```bash
+$ pip3 install catocli
+$ export CATO_TOKEN="your-api-token-here"
+$ export CATO_ACCOUNT_ID="your-account-id"
+$ catocli entity allocatedIP list
+```
+
+## Site Location Reference
+
+For more information on site_location syntax, use the [Cato CLI](https://github.com/catonetworks/cato-cli) to lookup values.
+
+```bash
+$ pip3 install catocli
+$ export CATO_TOKEN="your-api-token-here"
+$ export CATO_ACCOUNT_ID="your-account-id"
+$ catocli query siteLocation -h
+$ catocli query siteLocation '{"filters":[{"search": "San Diego","field":"city","operation":"exact"}]}' -p
+```
+
+## Authors
+
+Module is maintained by [Cato Networks](https://github.com/catonetworks) with help from [these awesome contributors](https://github.com/catonetworks/terraform-cato-ipsec-aws/graphs/contributors).
+
+## License
+
+Apache 2 Licensed. See [LICENSE](https://github.com/catonetworks/terraform-cato-ipsec-aws/tree/master/LICENSE) for full details.
+
---
+++ main.tf
@@ -0,0 +1,179 @@
+# Configure the Cato Provider
+provider "cato" {
+  baseurl    = var.baseurl
+  token      = var.token
+  account_id = var.account_id
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create 2 allocated IPs in Cato Management Application(CMA), Get IDs
+
+# Public IP for the VPN Gateway
+resource "azurerm_public_ip" "vpn_gateway_pip" {
+  name                = "${var.vpn_gateway_name}-publicip"
+  location            = var.az_location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# VPN Gateway
+
+resource "azurerm_virtual_network_gateway" "vpn_gateway" {
+  name                = var.vpn_gateway_name
+  location            = var.az_location
+  resource_group_name = var.resource_group_name
+
+  type     = "Vpn"
+  vpn_type = "RouteBased"
+
+  active_active = false
+  enable_bgp    = false
+  sku           = "VpnGw1"
+
+  ip_configuration {
+    name                          = "vnetGatewayConfig"
+    public_ip_address_id          = azurerm_public_ip.vpn_gateway_pip.id
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = var.gateway_subnet_id
+  }
+}
+
+# Local Network Gateway representing the Cato PoP
+
+resource "azurerm_local_network_gateway" "cato_pop_primary" {
+  name                = "${var.local_network_gateway_name}-primary"
+  location            = var.az_location
+  resource_group_name = var.resource_group_name
+  gateway_address     = var.primary_cato_pop_ip
+  address_space       = ["10.0.0.0/8"]
+}
+
+resource "azurerm_local_network_gateway" "cato_pop_secondary" {
+  name                = "${var.local_network_gateway_name}-secondary"
+  location            = var.az_location
+  resource_group_name = var.resource_group_name
+  gateway_address     = var.secondary_cato_pop_ip
+  address_space       = ["10.0.0.0/8"]
+}
+
+# Shared key for the VPN connection
+
+resource "random_password" "shared_key_primary" {
+  length  = 32
+  special = true
+}
+
+resource "random_password" "shared_key_secondary" {
+  length  = 32
+  special = true
+}
+
+# VPN Connection to Cato
+
+resource "azurerm_virtual_network_gateway_connection" "cato_connection_primary" {
+  name                = "${var.site_name}--connection-primary"
+  location            = var.az_location
+  resource_group_name = var.resource_group_name
+
+  type                       = "IPsec"
+  virtual_network_gateway_id = azurerm_virtual_network_gateway.vpn_gateway.id
+  local_network_gateway_id   = azurerm_local_network_gateway.cato_pop_primary.id
+
+  shared_key = var.primary_connection_shared_key==null ? random_password.shared_key_primary.result : var.primary_connection_shared_key
+}
+
+resource "azurerm_virtual_network_gateway_connection" "cato_connection_secondary" {
+  name                = "${var.site_name}-connection-secondary"
+  location            = var.az_location
+  resource_group_name = var.resource_group_name
+
+  type                       = "IPsec"
+  virtual_network_gateway_id = azurerm_virtual_network_gateway.vpn_gateway.id
+  local_network_gateway_id   = azurerm_local_network_gateway.cato_pop_secondary.id
+
+  shared_key = var.secondary_connection_shared_key==null ? random_password.shared_key_secondary.result : var.secondary_connection_shared_key
+}
+
+
+# Create Cato ipsec site and tunnels
+resource "cato_ipsec_site" "ipsec-site" {
+  name                 = var.site_name
+  site_type            = var.site_type
+  description          = var.site_description
+  native_network_range = var.native_network_range
+  site_location        = var.site_location
+  ipsec = {
+    primary = {
+      destination_type  = var.primary_destination_type
+      public_cato_ip_id = var.primary_public_cato_ip_id
+      pop_location_id   = var.primary_pop_location_id
+      tunnels = [
+        {
+          public_site_ip  = azurerm_public_ip.vpn_gateway_pip.ip_address
+          private_cato_ip = var.primary_private_cato_ip
+          private_site_ip = var.primary_private_site_ip
+          psk             = var.primary_connection_shared_key==null ? random_password.shared_key_primary.result : var.primary_connection_shared_key
+          last_mile_bw = {
+            downstream = var.downstream_bw
+            upstream   = var.upstream_bw
+          }
+        }
+      ]
+    }
+    secondary = {
+      destination_type  = var.secondary_destination_type
+      public_cato_ip_id = var.secondary_public_cato_ip_id
+      pop_location_id   = var.secondary_pop_location_id
+      tunnels = [
+        {
+          public_site_ip  = azurerm_public_ip.vpn_gateway_pip.ip_address
+          private_cato_ip = var.secondary_private_cato_ip
+          private_site_ip = var.secondary_private_site_ip
+          psk             = var.secondary_connection_shared_key==null ? random_password.shared_key_secondary.result : var.secondary_connection_shared_key
+          last_mile_bw = {
+            downstream = var.downstream_bw
+            upstream   = var.upstream_bw
+          }
+        }
+      ]
+    }
+  }
+}
+
+resource "null_resource" "update_dh_group" {
+  depends_on = [cato_ipsec_site.ipsec-site]
+  lifecycle {
+    ignore_changes = all
+  }
+  triggers = {
+    site_id = cato_ipsec_site.ipsec-site.id
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+curl -k -X POST \
+  -H "Accept: application/json" \
+  -H "Content-Type: application/json" \
+  -H "x-API-Key: ${var.token}" \
+  '${var.baseurl}' \
+  --data '{
+    "query": "mutation siteUpdateIpsecIkeV2SiteGeneralDetails($siteId: ID!, $updateIpsecIkeV2SiteGeneralDetailsInput: UpdateIpsecIkeV2SiteGeneralDetailsInput!, $accountId: ID!) { site(accountId: $accountId) { updateIpsecIkeV2SiteGeneralDetails(siteId: $siteId, input: $updateIpsecIkeV2SiteGeneralDetailsInput) { siteId localId } } }",
+    "variables": {
+      "accountId": ${var.account_id},
+      "siteId": "${cato_ipsec_site.ipsec-site.id}",
+      "updateIpsecIkeV2SiteGeneralDetailsInput": {
+        "initMessage": {
+          "dhGroup": "DH_2_MODP1024"
+        }
+      }
+    },
+    "operationName": "siteUpdateIpsecIkeV2SiteGeneralDetails"
+  }'
+EOF
+  }
+}
