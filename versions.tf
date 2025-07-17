32dfff1abac20eb181432a12c597c938265c2f03
Justin Richert <justin.richert@catonetworks.com>
2025-07-17 11:27:10 -0500

    Updates: 1. Update with latest site_location 2. Version Lock Provider with 0.0.30 or greater 3. Update Docs 4. Update ChangeLog

--- versions.tf
+++ versions.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     cato = {
       source  = "catonetworks/cato"
-      version = ">= 0.0.24"
+      version = ">= 0.0.30"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 4.1.0"
     }
   }
-  required_version = ">= 1.4"
+  required_version = ">= 1.5"
 }
